f3ebba85164506dad71e6af3ea0caf8a2f682f0e
Bike <iambike11@gmail.com>
2024-05-02 14:48:47 +0700

    input number

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
-
-
+        if (number <= 49){
+            System.out.println("grade F "+ number);
+        } else if (number > 49 && number <= 60){
+            System.out.println("grade D "+ number);
+        }
+        else if (number > 60 && number <= 69){
+            System.out.println("grade C "+ number);
+        }
+        else if (number > 69 && number <= 79){
+            System.out.println("grade B "+ number);
+        }
+        else {
+            System.out.println("grade A "+ number);
+        }
     }
 }
